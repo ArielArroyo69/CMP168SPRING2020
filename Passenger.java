637368a4dba7c926605a78a20bf02316fb98d723
ArielArroyo69 <54905552+ArielArroyo69@users.noreply.github.com>
2020-02-24 12:27:33 -0500

    Add files via upload

--- Passenger.java
+++ Passenger.java
@@ -14,9 +14,29 @@ public Passenger() {
 	gender = 'u';
 	numCarryOn = 0;
 }
+
+public Passenger(String name, int birthYear, double weight, char gender, int numCarryOn) {
+	if (numCarryOn<0)
+		numCarryOn = 0;
+	else if (numCarryOn>2)
+		numCarryOn = 2;
 	
-public Passenger (String name, int birthYear, double weight, char gender, int numCarryOn) {
+	if (weight<0)
+		weight = -1.0;
 	
+	if (gender == 'm')
+		gender = 'm';
+	else if (gender == 'f')
+		gender = 'f';
+	else
+		gender = 'u';
+		
+	
+	this.name = name;
+	this.birthYear = birthYear;
+	this.weight = weight;
+	this.gender = gender;
+	this.numCarryOn = numCarryOn;
 }
 
 public int calculateAge(int currentYear) {
@@ -29,11 +49,17 @@ public int calculateAge(int currentYear) {
 }
 
 public void gainWeight() {
-	weight += 1;
+	if (weight >= 1)
+		weight += 1;
 }
 
-public void gainWeight(double weight) {
-	weight += weight;
+public void gainWeight(double gainedWeight) {
+	if (gainedWeight>0.0)
+		weight += gainedWeight;
+	
+	if (weight<0)
+		weight = 0;
+	
 }
 
 public int getBirthYear() {
@@ -75,11 +101,47 @@ public void loseWeight() {
 		weight -= 1;
 }
 
-public void loseWeight(double weight) {
-	weight -= weight;
+public void loseWeight(double lostWeight) {
+		weight -= lostWeight;
+	
+	if (weight<0)
+		weight = 0;
 }
 
+public void printDetails() {
+	System.out.printf("Name: %20s | Year of Birth: %4d | Weight: %10.2f | Gender: %c | NumCarryOn: %2d\n", name, birthYear, weight, gender, numCarryOn);
+}
 
+public void setGender(char newGender) {
+	if (newGender == 'm')
+		gender = 'm';
+	else if (newGender == 'f')
+		gender = 'f';
+	else
+		gender = 'u';
+}
+
+public void setName(String sName) {
+	name = sName;
+}
+
+public void setBirthYear(int bYear) {
+	birthYear = bYear;
+}
+
+public void setWeight(double newWeight) {
+	if (newWeight<0)
+		weight = -1;
+	else
+		weight = newWeight;
+}
+
+public void setNumCarryOn(int newCarryOn) {
+	if (newCarryOn<0)
+		numCarryOn = 0;
+	else if (newCarryOn>2)
+		numCarryOn = 2;
+}
 
 	public static void main(String[] args) {
 		Passenger p1 = new Passenger();
